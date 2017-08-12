63811341be949be203c86af7407ffdec31d08c3d
juandjara <juanorigami@gmail.com>
2017-08-12 15:26:31 +0200

    feat: react to new members

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Composer, Extra, reply } = require('micro-bot')
 const bot = new Composer()
 
-const loremCat = 'https://lorempixel.com/400/200/cats/'
+const loremCat = 'http://thecatapi.com/api/images/get'
 const commandDoc = [
   {command: '/help', description: 'Lista de comandos disponibles (esta lista)'},
   {command: '/echo', description: 'Recibe un texto y lo repite'},
@@ -12,8 +12,7 @@ const helpText = commandDoc
   .map(c => `${c.command} ${c.description}`)
   .join('\n')
 const answerOfLife = `
-  *42* es es sentido de la vida,
-  el universo y *todo lo demas*`
+  *42* es es sentido de la vida, el universo y *todo lo demas*`
 
 bot.command('/start', reply('Usa /help para ver los comandos disponibles'))
 bot.command('/help', reply(helpText))
@@ -22,5 +21,19 @@ bot.command('/echo', ({reply, message}) => {
 })
 bot.command('/cat', ({replyWithPhoto}) => replyWithPhoto(loremCat))
 bot.command('/answer_of_life', reply(answerOfLife, Extra.markdown()))
+bot.on('message', ({reply, message}) => {
+  const { 
+    new_chat_members, 
+    left_chat_member 
+  } = message
+  if(new_chat_members) {
+    new_chat_members.forEach(user => {
+      reply(`Bienvenido @${user.username}`)
+    })
+  }
+  if(left_chat_member) {
+    reply(`Enga nos vemo ${left_chat_member.username}`)
+  }
+})
 
 module.exports = bot

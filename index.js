b87ea982ea16a8ad3a54ee0ac3a77d0fcfa9207f
juandjara <juanorigami@gmail.com>
2017-08-12 13:46:02 +0200

    refactor: command organization

--- index.js
+++ index.js
@@ -2,24 +2,25 @@ const { Composer, Extra, reply } = require('micro-bot')
 const bot = new Composer()
 
 const loremCat = 'https://lorempixel.com/400/200/cats/'
-const commands = {
-  echo: ctx => ctx.reply(ctx.message.text.replace('/echo', '')),
-  answer_of_life: reply(`
-      *42* es es sentido de la vida el universo y *todo lo demas*
-    `, Extra.markdown()),
-  cat: ({replyWithPhoto}) => replyWithPhoto(loremCat),
-}
+const commandDoc = [
+  {command: '/help', description: 'Lista de comandos disponibles (esta lista)'},
+  {command: '/echo', description: 'Recibe un texto y lo repite'},
+  {command: '/answer_of_life', description: 'Cual es el sentido de la vida'},
+  {command: '/cat', description: 'Fotos de gatos, el nucleo de internet'}
+]
+const helpText = commandDoc
+  .map(c => `${c.command} ${c.description}`)
+  .join('\n')
+const answerOfLife = `
+  *42* es es sentido de la vida,
+  el universo y *todo lo demas*`
 
 bot.command('/start', reply('Usa /help para ver los comandos disponibles'))
-
-Object.keys(commands).forEach(key => {
-  bot.command(key, commands[key])
-})
-
-const helpText = ['Comandos disponibles', '/help']
-  .concat(Object.keys(commands).map(c => "/"+c))
-  .join('\n')
-
 bot.command('/help', reply(helpText))
+bot.command('/echo', ({reply, message}) => {
+  reply(message.text.replace('/echo', ''))
+})
+bot.command('/cat', ({replyWithPhoto}) => replyWithPhoto(loremCat))
+bot.command('/answer_of_life', reply(answerOfLife, Extra.markdown()))
 
 module.exports = bot

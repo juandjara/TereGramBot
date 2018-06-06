35098e75b2d53aec5b7f7ad517b73f6ef65e33f0
IhToN <ihton93@gmail.com>
2018-06-06 23:52:49 +0200

    Not "const" variables to "let". ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const axios = require('axios')
-const donger = require('cool-ascii-faces')
-const { Composer, Extra, reply } = require('micro-bot')
-const bot = new Composer()
-const pkg = require('./package.json')
+const axios = require('axios');
+const donger = require('cool-ascii-faces');
+const { Composer, Extra, reply } = require('micro-bot');
+const bot = new Composer();
+const pkg = require('./package.json');
 
-const loremCat = 'http://thecatapi.com/api/images/get'
+const loremCat = 'http://thecatapi.com/api/images/get';
 const commandDoc = [
   {command: '/help', description: 'Lista de comandos disponibles (esta lista)'},
   {command: '/echo', description: 'Recibe un texto y lo repite'},
@@ -13,16 +13,16 @@ const commandDoc = [
   {command: '/repo', description: 'Enlace al repositorio de GitHub de este bot'},
   {command: '/status', description: 'Comprueba el estado de Tere'},
   {command: '/donger', description:  '( ͡° ͜ʖ ͡°). Puedes pasar un id despues del comando. Si no se seleccionara un donger aleatorio'},
-  {command: '/flame', description: 'illo callarse'}
-]
+  {command: '/flame', description: 'illo callarse'},
+];
 const helpText = commandDoc
   .map(c => `${c.command} ${c.description}`)
-  .join('\n')
-const answerOfLife = "Error code: *42*"
-const randomChoice = (choices) => {
-  const randomIndex = Date.now() % choices.length
-  return choices[randomIndex]
-}
+  .join('\n');
+const answerOfLife = "Error code: *42*";
+let randomChoice = (choices) => {
+  const randomIndex = Date.now() % choices.length;
+  return choices[randomIndex];
+};
 const teres = [
   'dime sosio',
   'al aparato',
@@ -36,66 +36,66 @@ const teres = [
   'te quiere i ya cansino',
   'que me dehe',
   'me va a come lo que tu sabe',
-  'no me cabrea pero me da coraje'
-]
-const startMsg = `Hola. Soy Tere. Tere Gram. Version ${pkg.version} Usa /help para ver los comandos disponibles`
+  'no me cabrea pero me da coraje',
+];
+const startMsg = `Hola. Soy Tere. Tere Gram. Version ${pkg.version} Usa /help para ver los comandos disponibles`;
 
-bot.command('/start', reply(startMsg))
-bot.command('/help', reply(helpText))
+bot.command('/start', reply(startMsg));
+bot.command('/help', reply(helpText));
 bot.command('/echo', ({reply, message}) => {
   const msg = message.text
     .replace('/echo', '')
-    .replace('@tere_gram_bot', '')
+    .replace('@tere_gram_bot', '');
   reply(msg)
-})
+});
 bot.command('/donger', ({message, reply}) => {
-  const commandHasIndex = /\/donger \d+$/.exec(message.text);
+  let commandHasIndex = /\/donger \d+$/.exec(message.text);
   let selectedDonger;
   let dongerIndex;
   if(commandHasIndex) {
-    dongerIndex = parseInt(/\d+$/.exec(message.text)[0])
+    dongerIndex = parseInt(/\d+$/.exec(message.text)[0]);
     if(dongerIndex >= donger.faces.length) {
-      reply(`Solo hay ${donger.faces.length - 1} dongers :c`)
-      return
+      reply(`Solo hay ${donger.faces.length - 1} dongers :c`);
+      return;
     }
     selectedDonger = donger.faces[dongerIndex]
   } else {
-    selectedDonger = donger()
-    dongerIndex = donger.faces.indexOf(selectedDonger)
+    selectedDonger = donger();
+    dongerIndex = donger.faces.indexOf(selectedDonger);
   }
   const msg = `
     Donger ${dongerIndex}:
     ${selectedDonger}
-  `
+  `;
   reply(msg)
-})
-bot.command('/status', ({reply}) => reply(randomChoice(teres)))
+});
+bot.command('/status', ({reply}) => reply(randomChoice(teres)));
 bot.command('/cat', async ({replyWithPhoto}) => {
-  const res = await axios.get(loremCat)
-  const trueCatUrl = res.request.res.responseUrl
+  let res = await axios.get(loremCat);
+  let trueCatUrl = res.request.res.responseUrl;
   replyWithPhoto(trueCatUrl)
-})
-bot.command('/answer_of_life', reply(answerOfLife, Extra.markdown()))
-bot.command('/repo', reply('https://github.com/juandjara/teregrambot'))
-bot.command('/flame', reply('illo chavale callarse que sus baneo a tos'))
+});
+bot.command('/answer_of_life', reply(answerOfLife, Extra.markdown()));
+bot.command('/repo', reply('https://github.com/juandjara/teregrambot'));
+bot.command('/flame', reply('illo chavale callarse que sus baneo a tos'));
 bot.on('message', ({reply, message}) => {
-  const { 
-    new_chat_members, 
+  let {
+    new_chat_members,
     left_chat_member,
     text
-  } = message
+  } = message;
   if(new_chat_members) {
     new_chat_members.forEach(user => {
       // check if the user has an ID created
-      let username = ""
+      let username = "";
       if (user.username) {
         username = `@${user.username}`
       } else {
         username = `${user.first_name}`
       }
-      
-      const msg = `Illo que pasa ${username}`
-      
+
+      let msg = `Illo que pasa ${username}`;
+
       if(user.username === 'tere_gram_bot') {
         msg = startMsg
       }
@@ -103,18 +103,18 @@ bot.on('message', ({reply, message}) => {
     })
   }
   if(left_chat_member) {
-    let username = ""
+    let username = "";
     if (user.username) {
       username = `@${left_chat_member.username}`
     } else {
       username = `${left_chat_member.first_name}`
     }
-    
+
     reply(`Enga nos vemo ${username}`)
   }
   if(/\btere\b/i.test(text)) {
-    reply(randomChoice(teres))
+    reply(randomChoice(teres));
   }
-})
+});
 
-module.exports = bot
+module.exports = bot;
